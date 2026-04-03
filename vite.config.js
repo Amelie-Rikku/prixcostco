de1af31414a59cba6395caad86792a4b20696794
Amelie-Rikku <catgirl.rikku@gmail.com>
2026-04-03 17:23:17 -0400

    fix: set base path for production build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/prixcostco/",
+  base: process.env.NODE_ENV === "production" ? "/prixcostco/" : "/",
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
 });
